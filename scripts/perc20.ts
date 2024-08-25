a4c5c5c8626269c254aad680a84c454a6ebec853
thanhren <40245250+thanhren@users.noreply.github.com>
2024-08-25 19:11:11 +0700

    Update perc20.ts

--- scripts/perc20.ts
+++ scripts/perc20.ts
@@ -56,7 +56,7 @@ async function main() {
   );
 
   // Write the transaction hash to contractperc20tx.txt
-  fs.writeFileSync(txFilePath, transfer.hash);
+  fs.writeFileSync(txFilePath, `https://explorer-evm.testnet.swisstronik.com/tx/${transfer.hash}`);
   console.log(`Transaction hash written to ${txFilePath}`);
 }
 
