1727e2a77f2a43887b05993a063e7c3ae11c5a7d
thanhren <40245250+thanhren@users.noreply.github.com>
2024-09-06 15:40:50 +0700

    Update done.sh
    Signed-off-by: thanhren <40245250+thanhren@users.noreply.github.com>

--- done.sh
+++ done.sh
@@ -19,7 +19,7 @@ cat "Task contract/contractperc20tx.txt" >> "Task contract/contracterc20.txt"
 cat "Task contract/contractprivate.txt" >> "Task contract/contracterc20.txt"
 
 # Append the content of Task contract/erc20tx.txt to erc20.txt
-cat "Task contract/contractroxy.txt" >> "Task contract/contracterc20.txt"
+cat "Task contract/contractproxy.txt" >> "Task contract/contracterc20.txt"
 
 # Append the content of Task contract/erc721.txt to erc20.txt
 cat "Task contract/contractproxytx.txt" >> "Task contract/contracterc20.txt"
